ada4def9c68d7e330549bb11b21c29c3271c3ad2
Sean Lynch <seanjcrl@gmail.com>
2018-06-30 12:49:39 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -3,5 +3,20 @@
 $(document).ready(function(){
 
 // call functions here
+function getIt() {
+  // bind click to p tag, on click, alert('Hey!');
+}
+
+function frameIt() {
+  // bind load event that adds class 'tasty' to image.
+}
+
+function pressIt() {
+  // bind keydown to input field, alert when press G key.
+}
+
+function submitIt() {
+  // bind submit to form alerts "Your form is going to be submitted now."
+}
 
 });
